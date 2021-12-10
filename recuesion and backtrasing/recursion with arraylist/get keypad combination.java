27eedddc297c410ef7f499cb0533117308bee4f5
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2021-12-10 12:29:18 +0530

    Create get keypad combination.java

---
+++ recuesion and backtrasing/recursion with arraylist/get keypad combination.java
@@ -0,0 +1,95 @@
+
+. You are given a string str. The string str will contains numbers only, where each number stands for a key pressed on a mobile phone.
+2. The following list is the key to characters map :
+    0 -> .;
+    1 -> abc
+    2 -> def
+    3 -> ghi
+    4 -> jkl
+    5 -> mno
+    6 -> pqrs
+    7 -> tu
+    8 -> vwx
+    9 -> yz
+3. Complete the body of getKPC function - without changing signature - to get the list of all words that could be produced by the keys in str.
+Use sample input and output to take idea about output.
+
+Note -> The online judge can't force you to write the function recursively but that is what the spirit of question is. Write recursive and not iterative logic. The purpose of the question is to aid learning recursion and not test you.                 
+
+Constraints
+
+0 <= str.length <= 10 str contains numbers only
+
+Format
+
+Input
+
+A string str
+
+Output
+
+Contents of the arraylist containing words as shown in sample output
+
+Example
+
+Sample Input
+
+78
+
+Sample Output
+
+[tv, tw, tx, uv, uw, ux]
+
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+
+   static String[] kpc={".;","abc","def","ghi","jkl","mno","pqrs","tu","vwx","yz"};
+
+    public static void main(String[] args) throws Exception {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.next();
+        ArrayList<String> al=getKPC(0,str);
+        System.out.println(al);
+
+    }
+
+    public static ArrayList<String> getKPC(int idx, String str) {
+
+            if(idx==str.length())
+            {
+                ArrayList<String> base =new ArrayList();
+                base.add("");
+                return base;
+
+            }
+
+            ArrayList<String> al=getKPC(idx+1,str); // faith that
+                // itwill give kepadpad combination from idx+1 to last 
+
+            ArrayList<String> anser= new ArrayList();
+            
+            String str1=kpc[str.charAt(idx)-'0'];
+
+
+            for(char st:str1.toCharArray())
+            {
+                for(String small:al)
+                {
+                    anser.add(st+small);
+                }
+
+            }
+
+            return anser;
+
+    }
+
+}
+
