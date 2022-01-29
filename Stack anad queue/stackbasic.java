d926c21230af973c288123a23ffc002199c05bc2
Umesh <92439041+Umeshchaurasiya123@users.noreply.github.com>
2022-01-29 12:25:16 +0530

    Create stackbasic.java

---
+++ Stack anad queue/stackbasic.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+public static void main(String [] args) throws Exception {
+ 
+  // note 
+  // stack can be created by using simple array , arraylist thay is mutable array and linked list
+  
+  // here we are using inbuild Stack<Wrapper class> stk=new Stack(); which is probided by java 
+  // it used ArrayList for creating stack 
+  
+  // push(data)
+  // pop()
+  //peek()
+  // it is all constant opration wile System.out.prinln(stk) is o(n) operation 
+  // beacuse it call .toString() method wich apply loop inside to get data
+  
+  
+ Scanner sc=new Scanner(System.in);
+ Stack<Integer> stk =new Stack();
+ // inicialization of an empty stack
+ 
+ stk.push(10);
+ System.out.println(stk.peek());
+ System.out.println(stk);
+ 
+ 
+ stk.push(20);
+ System.out.println(stk.peek());
+ System.out.println(stk);
+ 
+ 
+ stk.push(30);
+ System.out.println(stk.peek());
+ System.out.println(stk);
+ 
+ 
+ stk.push(40);
+ System.out.println(stk.peek());
+ System.out.println(stk);
+ 
+ stk.pop();
+ System.out.println(stk);
+ 
+ 
+ stk.pop();
+ System.out.println(stk);
+ 
+ stk.pop();
+ System.out.println(stk);
+ 
+ 
+ stk.pop();
+ System.out.println(stk);
+ 
+  
+  }
+}
